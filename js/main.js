4da23d3120be7d69124c443d72cf9b29930ed3e9
nomercy25 <tema.polivoda@gmail.com>
2023-03-16 18:00:23 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const circles = document.querySelectorAll(".circle");
+console.log(circles);
+let activeLight = 0;
+
+setInterval(() => {
+   changeLight();
+}, 3000);
+
+function changeLight() {
+   circles[activeLight].className = 'circle';
+   activeLight++;
+
+   if(activeLight > 2) {
+      activeLight = 0;
+   }
+   const currentLight = circles[activeLight];
+   currentLight.classList.add(currentLight.getAttribute("color"))
+}
